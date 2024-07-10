a33504ba1b259f2a75f41608ac286cff84901e38
Myo Min Thu <myominthu819@gmail.com>
2024-07-10 18:33:32 +0700

    updated

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
         element: <Home />
       },
       {
-        path: "/product-details",
+        path: "/shopping_app/product-details",
         element: <ProductDetails />
       }
     ]
